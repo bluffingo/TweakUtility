7bf7e1c6fa260a3061d7bb1fffffe9961632b5cb
Big Oof <45898787+BlocklandID47101@users.noreply.github.com>
2019-07-05 18:50:12 -0400

    Attempt at Windows SFC desactivation inclusion

--- TweakPages/AdvancedPage.cs
+++ TweakPages/AdvancedPage.cs
@@ -34,5 +34,26 @@ namespace TweakUtility.TweakPages
                 }
             }
         }
+
+        [DisplayName("Windows System File Checker (XP)")]
+        [Description("Defaults to false.")]
+        [OperatingSystemSupported(OperatingSystemVersion.WindowsXP)]
+        public bool WindowsSFC
+        {
+            get
+            {
+                using (RegistryKey subKey = Program.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\System"))
+                {
+                    return (int)subKey.GetValue("VerboseStatus", RegistryValueKind.DWord) == 1;
+                }
+            }
+            set
+            {
+                using (RegistryKey subKey = Program.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\System", true))
+                {
+                    subKey.SetValue("VerboseStatus", value ? 1 : 0, RegistryValueKind.DWord);
+                }
+            }
+        }
     }
 }
