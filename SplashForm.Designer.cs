9d0ecf5866c4d3e4cddb5d889b517275da4d7607
Big Oof <45898787+BlocklandID47101@users.noreply.github.com>
2019-07-06 18:13:27 -0400

    Smaller Splash

--- SplashForm.Designer.cs
+++ SplashForm.Designer.cs
@@ -39,9 +39,11 @@
             this.titleLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titleLabel.Location = new System.Drawing.Point(12, 9);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(372, 25);
+            this.titleLabel.Size = new System.Drawing.Size(281, 25);
             this.titleLabel.TabIndex = 0;
             this.titleLabel.Text = "Tweak Utility";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.titleLabel.UseWaitCursor = true;
             // 
             // statusLabel
             // 
@@ -50,13 +52,15 @@
             this.statusLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.statusLabel.Location = new System.Drawing.Point(13, 34);
             this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(371, 19);
+            this.statusLabel.Size = new System.Drawing.Size(280, 19);
             this.statusLabel.TabIndex = 1;
             this.statusLabel.Text = "Please wait...";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.statusLabel.UseWaitCursor = true;
             // 
             // SplashForm
             // 
-            this.ClientSize = new System.Drawing.Size(396, 66);
+            this.ClientSize = new System.Drawing.Size(305, 66);
             this.ControlBox = false;
             this.Controls.Add(this.statusLabel);
             this.Controls.Add(this.titleLabel);
