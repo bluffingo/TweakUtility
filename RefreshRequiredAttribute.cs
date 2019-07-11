70d9924eeaacf275819f08c3888993898216f762
Craftplacer <craftplacer@outlook.de>
2019-07-11 22:29:09 +0200

    Add default value to RefreshRequiredAttribute

--- RefreshRequiredAttribute.cs
+++ RefreshRequiredAttribute.cs
@@ -4,13 +4,18 @@ namespace TweakUtility
 {
     public class RefreshRequiredAttribute : Attribute
     {
-        public RefreshRequiredAttribute(RestartType type) => this.Type = type;
+        public RefreshRequiredAttribute(RestartType type = RestartType.None) => this.Type = type;
 
         public RestartType Type { get; set; }
     }
 
     public enum RestartType
     {
+        /// <summary>
+        /// Requires nothing to be done, to apply the changes.
+        /// </summary>
+        None,
+
         /// <summary>
         /// Requires the Windows Explorer to be restarted, to apply the changes.
         /// </summary>
