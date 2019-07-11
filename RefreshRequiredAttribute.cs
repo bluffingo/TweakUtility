20b0b06881171514aaf09c847c09fc28d6e47517
Craftplacer <craftplacer@outlook.de>
2019-07-11 21:45:28 +0200

    Implement attribute for change application dependencies

---
+++ RefreshRequiredAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TweakUtility
+{
+    public class RefreshRequiredAttribute : Attribute
+    {
+        public RefreshRequiredAttribute(RestartType type) => this.Type = type;
+
+        public RestartType Type { get; set; }
+    }
+
+    public enum RestartType
+    {
+        /// <summary>
+        /// Requires the Windows Explorer to be restarted, to apply the changes.
+        /// </summary>
+        ExplorerRestart,
+
+        /// <summary>
+        /// Requires the system to be restarted, to apply the changes.
+        /// </summary>
+        SystemRestart,
+
+        /// <summary>
+        /// Requires the user to be logged off, to apply the changes.
+        /// </summary>
+        Logoff,
+    }
+}
