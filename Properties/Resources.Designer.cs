fd2b568d60d3e2d24788ec7f4f568105af02c458
Big Oof <45898787+BlocklandID47101@users.noreply.github.com>
2019-07-12 14:50:44 -0400

    New icon
    Co-Authored-By: craftplacer <craftplacer@outlook.de>

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace TweakUtility.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon TweakUtility {
+            get {
+                object obj = ResourceManager.GetObject("TweakUtility", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
