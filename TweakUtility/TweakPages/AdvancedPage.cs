d49501ce10863b6ab313dceddf2d0b1a7bf4c091
Craftplacer <craftplacer@outlook.de>
2019-08-22 19:01:10 +0200

    Move sub pages into their own files

--- TweakUtility/TweakPages/AdvancedPage.cs
+++ TweakUtility/TweakPages/AdvancedPage.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.Win32;
 
-using System;
-using System.Diagnostics;
-using System.Drawing;
-
 using TweakUtility.Attributes;
-using TweakUtility.Enums;
 using TweakUtility.Helpers;
 
 /// TweakUtility - IMPORTANT NOTES
@@ -15,9 +10,9 @@ using TweakUtility.Helpers;
 
 namespace TweakUtility.TweakPages
 {
-    internal class AdvancedPage : TweakPage
+    internal partial class AdvancedPage : TweakPage
     {
-        internal AdvancedPage() : base("Advanced", new OEMInformation(), new DiskCleanupPage()) => this.Icon = NativeHelpers.ExtractIcon(@"%SystemRoot%\System32\shell32.dll", -22);
+        internal AdvancedPage() : base("Advanced", new OEMInformationPage(), new DiskCleanupPage()) => this.Icon = NativeHelpers.ExtractIcon(@"%SystemRoot%\System32\shell32.dll", -22);
 
         [DisplayName("Verbose Messages")]
         public bool VerboseMessages
@@ -96,134 +91,6 @@ namespace TweakUtility.TweakPages
 
             [Description("Enabled without prompts")]
             EnabledNoPrompt = 4
-        }
-
-        private class OEMInformation : TweakPage
-        {
-            public OEMInformation() : base("OEM Information")
-            {
-                this.Icon = Icons.Information;
-            }
-
-            public string Logo
-            {
-                get
-                {
-                    if (OperatingSystemVersions.IsSupported(OperatingSystemVersion.WindowsVista))
-                    {
-                        return RegistryHelper.GetValue<string>(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\Logo");
-                    }
-                    else
-                    {
-                        return Environment.ExpandEnvironmentVariables(@"%SystemRoot%\system32\OEMLogo.bmp");
-                    }
-                }
-
-                set
-                {
-                    if (OperatingSystemVersions.IsSupported(OperatingSystemVersion.WindowsVista))
-                    {
-                        RegistryHelper.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\Logo", value);
-                    }
-                    else
-                    {
-                        using (var image = Image.FromFile(value))
-                        {
-                            image.Save(@"%SystemRoot%\system32\OEMLogo.bmp");
-                        }
-                    }
-                }
-            }
-
-            public string Manufacturer
-            {
-                get
-                {
-                    if (OperatingSystemVersions.IsSupported(OperatingSystemVersion.WindowsVista))
-                    {
-                        return RegistryHelper.GetValue<string>(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\Manufacturer");
-                    }
-                    else
-                    {
-                        return NativeHelpers.IniReadValue("General", "Manufacturer", @"%SystemRoot%\System32\OEMInfo.ini");
-                    }
-                }
-
-                set
-                {
-                    if (OperatingSystemVersions.IsSupported(OperatingSystemVersion.WindowsVista))
-                    {
-                        RegistryHelper.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\Manufacturer", value);
-                    }
-                    else
-                    {
-                        NativeHelpers.IniWriteValue("General", "Manufacturer", value, @"%SystemRoot%\System32\OEMInfo.ini");
-                    }
-                }
-            }
-
-            public string Model
-            {
-                get
-                {
-                    if (OperatingSystemVersions.IsSupported(OperatingSystemVersion.WindowsVista))
-                    {
-                        return RegistryHelper.GetValue<string>(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\Model");
-                    }
-                    else
-                    {
-                        return NativeHelpers.IniReadValue("General", "Model", @"%SystemRoot%\System32\OEMInfo.ini");
-                    }
-                }
-                set
-                {
-                    if (OperatingSystemVersions.IsSupported(OperatingSystemVersion.WindowsVista))
-                    {
-                        RegistryHelper.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\Model", value);
-                    }
-                    else
-                    {
-                        NativeHelpers.IniWriteValue("General", "Model", value, @"%SystemRoot%\System32\OEMInfo.ini");
-                    }
-                }
-            }
-
-            [OperatingSystemSupported(OperatingSystemVersion.WindowsVista)]
-            [DisplayName("Support hours")]
-            public string SupportHours
-            {
-                get => RegistryHelper.GetValue<string>(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\SupportHours");
-                set => RegistryHelper.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\SupportHours", value);
-            }
-
-            [OperatingSystemSupported(OperatingSystemVersion.WindowsVista)]
-            [DisplayName("Support phone number")]
-            public string SupportPhone
-            {
-                get => RegistryHelper.GetValue<string>(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\SupportPhone");
-                set => RegistryHelper.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\SupportPhone", value);
-            }
-
-            [OperatingSystemSupported(OperatingSystemVersion.WindowsVista)]
-            [DisplayName("Support URL")]
-            public string SupportURL
-            {
-                get => RegistryHelper.GetValue<string>(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\SupportURL");
-                set => RegistryHelper.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\SupportURL", value);
-            }
-
-            [Visible(true)]
-            public void Preview()
-            {
-                if (OperatingSystemVersions.IsSupported(OperatingSystemVersion.WindowsVista))
-                {
-                    Process.Start("control.exe", "system");
-                }
-                else
-                {
-                    Process.Start(new ProcessStartInfo("sysdm.cpl") { UseShellExecute = true });
-                }
-            }
         }
     }
 }
