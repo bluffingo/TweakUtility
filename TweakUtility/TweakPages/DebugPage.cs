843e15ed978235f17a776d7ee78fdb591a237b17
Craftplacer <craftplacer@outlook.de>
2019-08-27 04:49:56 +0200

    Support displaying transparent colors

--- TweakUtility/TweakPages/DebugPage.cs
+++ TweakUtility/TweakPages/DebugPage.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 using TweakUtility.Attributes;
 using TweakUtility.Theming;
 
@@ -14,6 +15,8 @@ namespace TweakUtility.TweakPages
         public string Text { get; set; }
         public int Integer { get; set; }
         public bool Boolean { get; set; }
+
+		public Color Color => Color.FromArgb(128, 255, 0, 0);
 
         [Visible(true)]
         public void Function() => MessageBox.Show("Hello world!");
