4af56adfef33761fb29e432513e5b6cea4f57f61
Craftplacer <craftplacer@outlook.de>
2019-09-01 16:20:09 +0200

    Reformat Theme.cs

--- TweakUtility/Theming/Theme.cs
+++ TweakUtility/Theming/Theme.cs
@@ -11,130 +11,123 @@ using static TweakUtility.Helpers.OperatingSystemVersions;
 
 namespace TweakUtility.Theming
 {
-    internal static class Theme
-    {
-        /// <summary>
-        /// Checks if the current system theme is dark.
-        /// </summary>
-        public static bool IsDark => SystemColors.Window.GetReadableColor();
+	internal static class Theme
+	{
+		/// <summary>
+		/// Checks if the current system theme is dark.
+		/// </summary>
+		public static bool IsDark => SystemColors.Window.GetReadableColor();
 
-        public static Color ApplicationBackground
-        {
-            get
-            {
-                if (IsSupportedCosmetic(OperatingSystemVersion.WindowsVista))
-                {
-                    return SystemColors.Window; //basically white on aero, no plans for aero transparency (like CustomizerGod) at the moment.
-                }
-                return SystemColors.Control;
-            }
-        }
+		public static Color ApplicationBackground
+		{
+			get
+			{
+				if (IsSupportedCosmetic(OperatingSystemVersion.WindowsVista))
+				{
+					return SystemColors.Window; //basically white on aero, no plans for aero transparency (like CustomizerGod) at the moment.
+				}
+				return SystemColors.Control;
+			}
+		}
 
-        public static Color BottomPanelBackground => SystemColors.Control;
+		public static Color BottomPanelBackground => SystemColors.Control;
 
-        public static Color LinkForeground
-        {
-            get
-            {
-                if (IsSupportedCosmetic(OperatingSystemVersion.WindowsVista))
-                {
-                    return Color.FromArgb(0, 102, 204);
-                }
+		public static Color LinkForeground
+		{
+			get
+			{
+				if (IsSupportedCosmetic(OperatingSystemVersion.WindowsVista))
+				{
+					return Color.FromArgb(0, 102, 204);
+				}
 
-                return Color.Black;
-            }
-        }
+				return Color.Black;
+			}
+		}
 
-        public static Color SidebarBackground;
+		public static Color SidebarBackground;
 
-        public static Color TitleForeground
-        {
-            get
-            {
-                if (IsDark)
-                {
-                    return Color.White;
-                }
+		public static Color TitleForeground
+		{
+			get
+			{
+				if (IsDark)
+				{
+					return Color.White;
+				}
 
-                if (IsSupportedCosmetic(OperatingSystemVersion.WindowsVista))
-                {
-                    return Color.FromArgb(0, 51, 153);
-                }
+				if (IsSupportedCosmetic(OperatingSystemVersion.WindowsVista))
+				{
+					return Color.FromArgb(0, 51, 153);
+				}
 
-                return Color.Black;
-            }
-        }
+				return Color.Black;
+			}
+		}
 
-        public static Color CategoryForeground => TitleForeground;
+		public static Color CategoryForeground => TitleForeground;
 
-        public static FontFamily TitleFontFamily
-        {
-            get
-            {
-                // Aero Theme
-                if (IsSupportedCosmetic(OperatingSystemVersion.WindowsVista))
-                {
-                    return FontFamily.Families.First(f => f.Name == "Segoe UI");
-                }
 
-                // Luna Theme
-                return FontFamily.Families.First(f => f.Name == "Franklin Gothic Medium");
-            }
-        }
+		public static FontFamily TitleFontFamily
+		{
+			get
+			{
+				// Aero Theme
+				if (IsSupportedCosmetic(OperatingSystemVersion.WindowsVista))
+				{
+					return FontFamily.Families.First(f => f.Name == "Segoe UI");
+				}
 
-        public static Font TitleFont => new Font(TitleFontFamily, TitleSize, GraphicsUnit.Point);
+				// Luna Theme
+				return FontFamily.Families.First(f => f.Name == "Franklin Gothic Medium");
+			}
+		}
 
-        public static Font CategoryFont => new Font(CategoryFontFamily, CategorySize, GraphicsUnit.Point);
+		public static Font TitleFont => new Font(TitleFontFamily, TitleSize, GraphicsUnit.Point);
 
-        public static FontFamily CategoryFontFamily
-        {
-            get
-            {
-                if (IsSupportedCosmetic(OperatingSystemVersion.WindowsVista))
-                {
-                    return FontFamily.Families.First(f => f.Name == "Segoe UI");
-                }
+		public static Font CategoryFont => new Font(CategoryFontFamily, CategorySize, GraphicsUnit.Point);
 
-                ///"Tahoma is used as the system's default font.
-                ///Tahoma should be used at 8, 9 or 11 point sizes."
-                ///
-                /// - Windows XP Visual Guidelines (http://interface.free.fr/Archives/GUI_Xp.pdf)
-                return FontFamily.Families.First(f => f.Name == "Tahoma");
-            }
-        }
+		public static FontFamily CategoryFontFamily
+		{
+			get
+			{
+				if (IsSupportedCosmetic(OperatingSystemVersion.WindowsVista))
+				{
+					return FontFamily.Families.First(f => f.Name == "Segoe UI");
+				}
 
-        public static int TitleSize => 14;
+				///"Tahoma is used as the system's default font.
+				///Tahoma should be used at 8, 9 or 11 point sizes."
+				///
+				/// - Windows XP Visual Guidelines (http://interface.free.fr/Archives/GUI_Xp.pdf)
+				return FontFamily.Families.First(f => f.Name == "Tahoma");
+			}
+		}
 
-        public static int CategorySize => 11;
+		public static int TitleSize => 14;
 
-        public static void Apply(Control control)
-        {
-            if (control == null)
-            {
-                throw new ArgumentNullException(nameof(control));
-            }
+		public static int CategorySize => 11;
 
-            if (control is Form form)
-            {
-                form.BackColor = ApplicationBackground;
-            }
+		public static void Apply(Control control)
+		{
+			if (control == null)
+				throw new ArgumentNullException(nameof(control));
 
-            if (control is MainForm mainForm)
-            {
-                mainForm.bottomPanel.BackColor = BottomPanelBackground;
-                mainForm.treeView.BackColor = SidebarBackground;
-            }
+			if (control is Form form)
+				form.BackColor = ApplicationBackground;
 
-            // are we going to do #2 later? yeah why not
-            if (control is LinkLabel linkLabel)
-            {
-                linkLabel.LinkColor = LinkForeground;
-            }
+			if (control is MainForm mainForm)
+			{
+				mainForm.bottomPanel.BackColor = BottomPanelBackground;
+				mainForm.treeView.BackColor = SidebarBackground;
+			}
 
-            foreach (Control subControl in control.Controls)
-            {
-                Apply(subControl);
-            }
-        }
-    }
+			if (control is LinkLabel linkLabel)
+				linkLabel.LinkColor = LinkForeground;
+
+			// Apply to sub-controls as well.
+			foreach (Control subControl in control.Controls)
+				Apply(subControl);
+		}
+	}
 }
