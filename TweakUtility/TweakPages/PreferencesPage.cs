8d0224b72a798c00c652c57e84d61f1eee6d7495
Craftplacer <craftplacer@outlook.de>
2019-09-01 16:20:37 +0200

    Reformat PreferencesPage.cs

--- TweakUtility/TweakPages/PreferencesPage.cs
+++ TweakUtility/TweakPages/PreferencesPage.cs
@@ -5,54 +5,54 @@ using TweakUtility.Helpers;
 
 namespace TweakUtility.TweakPages
 {
-    public class PreferencesPage : TweakPage
-    {
-        public PreferencesPage() : base("Tweak Utility Preferences")
-        {
-        }
+	public class PreferencesPage : TweakPage
+	{
+		public PreferencesPage() : base("Tweak Utility Preferences")
+		{
+		}
 
-        [DisplayName("Automatically install extensions")]
-        public bool AutoInstallExtensions
-        {
-            get => Properties.Settings.Default.AutoInstallExtensions;
-            set => Properties.Settings.Default.AutoInstallExtensions = value;
-        }
+		[DisplayName("Automatically install extensions")]
+		public bool AutoInstallExtensions
+		{
+			get => Properties.Settings.Default.AutoInstallExtensions;
+			set => Properties.Settings.Default.AutoInstallExtensions = value;
+		}
 
-        [DisplayName("Cosmetic verison")]
-        [Description("This preference will override some OS-specific styling values")]
-        public static OperatingSystemVersion CosmeticVersion
-        {
-            get => Properties.Settings.Default.CosmeticVersion;
-            set => Properties.Settings.Default.CosmeticVersion = value;
-        }
+		[DisplayName("Cosmetic verison")]
+		[Description("This preference will override some OS-specific styling values")]
+		public static OperatingSystemVersion CosmeticVersion
+		{
+			get => Properties.Settings.Default.CosmeticVersion;
+			set => Properties.Settings.Default.CosmeticVersion = value;
+		}
 
-        /// <summary>
-        /// Intended to be used by code.
-        /// </summary>
-        [Visible(false)]
-        public static Version GetCosmeticVersion()
-        {
-            if (CosmeticVersion == OperatingSystemVersion.None)
-            {
-                return OperatingSystemVersions.CurrentVersion;
-            }
+		/// <summary>
+		/// Intended to be used by code.
+		/// </summary>
+		[Visible(false)]
+		public static Version GetCosmeticVersion()
+		{
+			if (CosmeticVersion == OperatingSystemVersion.None)
+			{
+				return OperatingSystemVersions.CurrentVersion;
+			}
 
-            return OperatingSystemVersions.GetVersion(CosmeticVersion);
-        }
+			return OperatingSystemVersions.GetVersion(CosmeticVersion);
+		}
 
-        [RefreshRequired(RestartType.TweakUtility)]
-        [DisplayName("Enable experimental features")]
-        public static bool EnableExperimentalFeatures
-        {
-            get => Properties.Settings.Default.EnableExperimentalFeatures;
-            set => Properties.Settings.Default.EnableExperimentalFeatures = value;
-        }
-    }
+		[RefreshRequired(RestartType.TweakUtility)]
+		[DisplayName("Enable experimental features")]
+		public static bool EnableExperimentalFeatures
+		{
+			get => Properties.Settings.Default.EnableExperimentalFeatures;
+			set => Properties.Settings.Default.EnableExperimentalFeatures = value;
+		}
+
 		[DisplayName("Save window position and size")]
 		public static bool SaveWindowInfo
 		{
 			get => Properties.Settings.Default.SaveWindowInfo;
 			set => Properties.Settings.Default.SaveWindowInfo = value;
 		}
-
+	}
 }
