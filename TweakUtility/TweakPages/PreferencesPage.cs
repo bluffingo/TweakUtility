5a6f9f65c0619dcff7d2a42684333749c8d168ce
Craftplacer <craftplacer@outlook.de>
2019-09-01 18:02:26 +0200

    "Redesign" the preferences page

--- TweakUtility/TweakPages/PreferencesPage.cs
+++ TweakUtility/TweakPages/PreferencesPage.cs
@@ -11,6 +11,7 @@ namespace TweakUtility.TweakPages
 		{
 		}
 
+		[Category("Behaviour")]
 		[DisplayName("Automatically install extensions")]
 		public bool AutoInstallExtensions
 		{
@@ -18,12 +19,39 @@ namespace TweakUtility.TweakPages
 			set => Properties.Settings.Default.AutoInstallExtensions = value;
 		}
 
+		[Category("Behaviour")]
+		[RefreshRequired(RestartType.TweakUtility)]
+		[DisplayName("Enable experimental features")]
+		public static bool EnableExperimentalFeatures
+		{
+			get => Properties.Settings.Default.EnableExperimentalFeatures;
+			set => Properties.Settings.Default.EnableExperimentalFeatures = value;
+		}
+
+		[Category("Behaviour")]
+		[DisplayName("Save window position and size")]
+		public static bool SaveWindowInfo
+		{
+			get => Properties.Settings.Default.SaveWindowInfo;
+			set => Properties.Settings.Default.SaveWindowInfo = value;
+		}
+
+		[Category("User Interface")]
 		[DisplayName("Cosmetic verison")]
 		[Description("This preference will override some OS-specific styling values")]
 		public static OperatingSystemVersion CosmeticVersion
 		{
 			get => Properties.Settings.Default.CosmeticVersion;
 			set => Properties.Settings.Default.CosmeticVersion = value;
+		}
+
+		[Category("User Interface")]
+		[DisplayName("Prefer sliders")]
+		[Description("Makes number values with a specified range turn into sliders")]
+		public static bool PreferSliders
+		{
+			get => Properties.Settings.Default.PreferSliders;
+			set => Properties.Settings.Default.PreferSliders = value;
 		}
 
 		/// <summary>
@@ -38,29 +66,6 @@ namespace TweakUtility.TweakPages
 			}
 
 			return OperatingSystemVersions.GetVersion(CosmeticVersion);
-		}
-
-		[RefreshRequired(RestartType.TweakUtility)]
-		[DisplayName("Enable experimental features")]
-		public static bool EnableExperimentalFeatures
-		{
-			get => Properties.Settings.Default.EnableExperimentalFeatures;
-			set => Properties.Settings.Default.EnableExperimentalFeatures = value;
-		}
-
-		[DisplayName("Prefer sliders")]
-		[Description("Makes number values with a specified range turn into sliders")]
-		public static bool PreferSliders
-		{
-			get => Properties.Settings.Default.PreferSliders;
-			set => Properties.Settings.Default.PreferSliders = value;
-		}
-
-		[DisplayName("Save window position and size")]
-		public static bool SaveWindowInfo
-		{
-			get => Properties.Settings.Default.SaveWindowInfo;
-			set => Properties.Settings.Default.SaveWindowInfo = value;
 		}
 	}
 }
