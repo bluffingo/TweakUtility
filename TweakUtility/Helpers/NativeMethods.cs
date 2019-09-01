61487e7c3f1b76ee31bf89688af103a0d1a0c500
Craftplacer <craftplacer@outlook.de>
2019-09-01 16:19:44 +0200

    Add DWM native methods and reformat

--- TweakUtility/Helpers/NativeMethods.cs
+++ TweakUtility/Helpers/NativeMethods.cs
@@ -5,172 +5,253 @@ using System.Text;
 
 namespace TweakUtility.Helpers
 {
-    internal static class NativeMethods
-    {
-        internal const int RT_GROUP_ICON = 14;
+	internal static class NativeMethods
+	{
+		internal const int RT_GROUP_ICON = 14;
 
-        internal const int RT_ICON = 0x00000003;
+		internal const int RT_ICON = 0x00000003;
 
-        internal const int WM_USER = 0x0400;
+		internal const int WM_USER = 0x0400;
 
-        [Flags]
-        internal enum ExitWindows : uint
-        {
-            // ONE of the following five:
-            LogOff = 0x00,
+		[Flags]
+		internal enum ExitWindows : uint
+		{
+			// ONE of the following five:
+			LogOff = 0x00,
 
-            ShutDown = 0x01,
-            Reboot = 0x02,
-            PowerOff = 0x08,
-            RestartApps = 0x40,
+			ShutDown = 0x01,
+			Reboot = 0x02,
+			PowerOff = 0x08,
+			RestartApps = 0x40,
 
-            // plus AT MOST ONE of the following two:
-            Force = 0x04,
+			// plus AT MOST ONE of the following two:
+			Force = 0x04,
 
-            ForceIfHung = 0x10,
-        }
+			ForceIfHung = 0x10,
+		}
 
-        [Flags]
-        internal enum LoadLibraryFlags : uint
-        {
-            None = 0,
-            DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
-            LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010,
-            LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
-            LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040,
-            LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020,
-            LOAD_LIBRARY_SEARCH_APPLICATION_DIR = 0x00000200,
-            LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000,
-            LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR = 0x00000100,
-            LOAD_LIBRARY_SEARCH_SYSTEM32 = 0x00000800,
-            LOAD_LIBRARY_SEARCH_USER_DIRS = 0x00000400,
-            LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008
-        }
+		[Flags]
+		internal enum LoadLibraryFlags : uint
+		{
+			None = 0,
+			DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
+			LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010,
+			LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
+			LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040,
+			LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020,
+			LOAD_LIBRARY_SEARCH_APPLICATION_DIR = 0x00000200,
+			LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000,
+			LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR = 0x00000100,
+			LOAD_LIBRARY_SEARCH_SYSTEM32 = 0x00000800,
+			LOAD_LIBRARY_SEARCH_USER_DIRS = 0x00000400,
+			LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008
+		}
 
-        [Flags]
-        internal enum ShutdownReason : uint
-        {
-            MajorApplication = 0x00040000,
-            MajorHardware = 0x00010000,
-            MajorLegacyApi = 0x00070000,
-            MajorOperatingSystem = 0x00020000,
-            MajorOther = 0x00000000,
-            MajorPower = 0x00060000,
-            MajorSoftware = 0x00030000,
-            MajorSystem = 0x00050000,
+		[Flags]
+		internal enum ShutdownReason : uint
+		{
+			MajorApplication = 0x00040000,
+			MajorHardware = 0x00010000,
+			MajorLegacyApi = 0x00070000,
+			MajorOperatingSystem = 0x00020000,
+			MajorOther = 0x00000000,
+			MajorPower = 0x00060000,
+			MajorSoftware = 0x00030000,
+			MajorSystem = 0x00050000,
 
-            MinorBlueScreen = 0x0000000F,
-            MinorCordUnplugged = 0x0000000b,
-            MinorDisk = 0x00000007,
-            MinorEnvironment = 0x0000000c,
-            MinorHardwareDriver = 0x0000000d,
-            MinorHotfix = 0x00000011,
-            MinorHung = 0x00000005,
-            MinorInstallation = 0x00000002,
-            MinorMaintenance = 0x00000001,
-            MinorMMC = 0x00000019,
-            MinorNetworkConnectivity = 0x00000014,
-            MinorNetworkCard = 0x00000009,
-            MinorOther = 0x00000000,
-            MinorOtherDriver = 0x0000000e,
-            MinorPowerSupply = 0x0000000a,
-            MinorProcessor = 0x00000008,
-            MinorReconfig = 0x00000004,
-            MinorSecurity = 0x00000013,
-            MinorSecurityFix = 0x00000012,
-            MinorSecurityFixUninstall = 0x00000018,
-            MinorServicePack = 0x00000010,
-            MinorServicePackUninstall = 0x00000016,
-            MinorTermSrv = 0x00000020,
-            MinorUnstable = 0x00000006,
-            MinorUpgrade = 0x00000003,
-            MinorWMI = 0x00000015,
+			MinorBlueScreen = 0x0000000F,
+			MinorCordUnplugged = 0x0000000b,
+			MinorDisk = 0x00000007,
+			MinorEnvironment = 0x0000000c,
+			MinorHardwareDriver = 0x0000000d,
+			MinorHotfix = 0x00000011,
+			MinorHung = 0x00000005,
+			MinorInstallation = 0x00000002,
+			MinorMaintenance = 0x00000001,
+			MinorMMC = 0x00000019,
+			MinorNetworkConnectivity = 0x00000014,
+			MinorNetworkCard = 0x00000009,
+			MinorOther = 0x00000000,
+			MinorOtherDriver = 0x0000000e,
+			MinorPowerSupply = 0x0000000a,
+			MinorProcessor = 0x00000008,
+			MinorReconfig = 0x00000004,
+			MinorSecurity = 0x00000013,
+			MinorSecurityFix = 0x00000012,
+			MinorSecurityFixUninstall = 0x00000018,
+			MinorServicePack = 0x00000010,
+			MinorServicePackUninstall = 0x00000016,
+			MinorTermSrv = 0x00000020,
+			MinorUnstable = 0x00000006,
+			MinorUpgrade = 0x00000003,
+			MinorWMI = 0x00000015,
 
-            FlagUserDefined = 0x40000000,
-            FlagPlanned = 0x80000000
-        }
+			FlagUserDefined = 0x40000000,
+			FlagPlanned = 0x80000000
+		}
 
-        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
-        public static extern int SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, IntPtr hToken, out string pszPath);
+		[DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+		public static extern int SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, IntPtr hToken, out string pszPath);
 
-        [DllImport("user32.dll")]
-        internal static extern IntPtr CreateIconFromResourceEx(byte[] pbIconBits, uint cbIconBits, bool fIcon, uint dwVersion, int cxDesired, int cyDesired, uint uFlags);
+		[DllImport("user32.dll")]
+		internal static extern IntPtr CreateIconFromResourceEx(byte[] pbIconBits, uint cbIconBits, bool fIcon, uint dwVersion, int cxDesired, int cyDesired, uint uFlags);
 
-        [DllImport("gdi32.dll", EntryPoint = "DeleteObject", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
-        internal static extern bool DeleteObject(IntPtr hBitmap);
+		[DllImport("gdi32.dll", EntryPoint = "DeleteObject", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+		internal static extern bool DeleteObject(IntPtr hBitmap);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool ExitWindowsEx(ExitWindows uFlags, ShutdownReason dwReason);
+		[DllImport("user32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		internal static extern bool ExitWindowsEx(ExitWindows uFlags, ShutdownReason dwReason);
 
-        [DllImport("Shell32.dll", EntryPoint = "ExtractIconExW", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
-        internal static extern int ExtractIconEx(string sFile, int iIndex, out IntPtr piLargeVersion, out IntPtr piSmallVersion, int amountIcons);
+		[DllImport("Shell32.dll", EntryPoint = "ExtractIconExW", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+		internal static extern int ExtractIconEx(string sFile, int iIndex, out IntPtr piLargeVersion, out IntPtr piSmallVersion, int amountIcons);
 
-        [DllImport("kernel32.dll")]
-        internal static extern IntPtr FindResource(IntPtr hModule, int lpName, int lpType);
+		[DllImport("kernel32.dll")]
+		internal static extern IntPtr FindResource(IntPtr hModule, int lpName, int lpType);
 
-        [DllImport("kernel32.dll")]
-        internal static extern IntPtr FindResource(IntPtr hModule, int lpID, string lpType);
+		[DllImport("kernel32.dll")]
+		internal static extern IntPtr FindResource(IntPtr hModule, int lpID, string lpType);
 
-        [DllImport("kernel32.dll")]
-        internal static extern IntPtr FindResourceEx(IntPtr hModule, IntPtr lpType, IntPtr lpName, ushort wLanguage);
+		[DllImport("kernel32.dll")]
+		internal static extern IntPtr FindResourceEx(IntPtr hModule, IntPtr lpType, IntPtr lpName, ushort wLanguage);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        internal static extern IntPtr FindWindow([MarshalAs(UnmanagedType.LPWStr)] string lpClassName, string lpWindowName);
+		[DllImport("user32.dll", SetLastError = true)]
+		internal static extern IntPtr FindWindow([MarshalAs(UnmanagedType.LPWStr)] string lpClassName, string lpWindowName);
 
-        [DllImport("kernel32.dll")]
-        internal static extern int FreeLibrary(IntPtr hLibModule);
+		[DllImport("kernel32.dll")]
+		internal static extern int FreeLibrary(IntPtr hLibModule);
 
-        [DllImport("User32.dll")]
-        internal static extern IntPtr LoadBitmap(IntPtr hInstance, int uID);
+		[DllImport("User32.dll")]
+		internal static extern IntPtr LoadBitmap(IntPtr hInstance, int uID);
 
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        internal static extern IntPtr LoadImage(IntPtr hinst, string lpszName, uint uType, int cxDesired, int cyDesired, uint fuLoad);
+		[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		internal static extern IntPtr LoadImage(IntPtr hinst, string lpszName, uint uType, int cxDesired, int cyDesired, uint fuLoad);
 
-        [DllImport("User32.dll")]
-        internal static extern IntPtr LoadImage(IntPtr hInstance, int uID, uint type, int width, int height, int load);
+		[DllImport("User32.dll")]
+		internal static extern IntPtr LoadImage(IntPtr hInstance, int uID, uint type, int width, int height, int load);
 
-        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
-        internal static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)]string lpFileName);
+		[DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
+		internal static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)]string lpFileName);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        internal static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, LoadLibraryFlags dwFlags);
+		[DllImport("kernel32.dll", SetLastError = true)]
+		internal static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, LoadLibraryFlags dwFlags);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        internal static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, uint dwFlags);
+		[DllImport("kernel32.dll", SetLastError = true)]
+		internal static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, uint dwFlags);
 
-        //http://msdn.microsoft.com/en-us/library/windows/desktop/ms644931(v=vs.85).aspx5).aspx
-        [DllImport("kernel32.dll", SetLastError = true)]
-        internal static extern IntPtr LoadResource(IntPtr hModule, IntPtr hResInfo);
+		//http://msdn.microsoft.com/en-us/library/windows/desktop/ms644931(v=vs.85).aspx5).aspx
+		[DllImport("kernel32.dll", SetLastError = true)]
+		internal static extern IntPtr LoadResource(IntPtr hModule, IntPtr hResInfo);
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        internal static extern int LoadString(IntPtr hInstance, int ID, StringBuilder lpBuffer, int nBufferMax);
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		internal static extern int LoadString(IntPtr hInstance, int ID, StringBuilder lpBuffer, int nBufferMax);
 
-        [DllImport("kernel32.dll")]
-        internal static extern IntPtr LockResource(IntPtr hResData);
+		[DllImport("kernel32.dll")]
+		internal static extern IntPtr LockResource(IntPtr hResData);
 
-        [DllImport("user32.dll")]
-        internal static extern int LookupIconIdFromDirectoryEx(byte[] presbits, bool fIcon, int cxDesired, int cyDesired, uint Flags);
+		[DllImport("user32.dll")]
+		internal static extern int LookupIconIdFromDirectoryEx(byte[] presbits, bool fIcon, int cxDesired, int cyDesired, uint Flags);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        internal static extern bool PostMessage(IntPtr hWnd, [MarshalAs(UnmanagedType.U4)] uint Msg, IntPtr wParam, IntPtr lParam);
+		[DllImport("user32.dll", SetLastError = true)]
+		internal static extern bool PostMessage(IntPtr hWnd, [MarshalAs(UnmanagedType.U4)] uint Msg, IntPtr wParam, IntPtr lParam);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        internal static extern uint SizeofResource(IntPtr hModule, IntPtr hResInfo);
+		[DllImport("kernel32.dll", SetLastError = true)]
+		internal static extern uint SizeofResource(IntPtr hModule, IntPtr hResInfo);
 
-        [DllImport("kernel32")]
-        internal static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+		[DllImport("kernel32")]
+		internal static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
 
-        [DllImport("kernel32")]
-        internal static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+		[DllImport("kernel32")]
+		internal static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
 
-        public const int HWND_BROADCAST = 0xffff;
-        public static readonly int WM_SHOWME = RegisterWindowMessage("WM_SHOWME");
+		public const int HWND_BROADCAST = 0xffff;
+		public static readonly int WM_SHOWME = RegisterWindowMessage("WM_SHOWME");
 
-        [DllImport("user32")]
-        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+		[DllImport("user32")]
+		public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
 
-        [DllImport("user32")]
-        public static extern int RegisterWindowMessage(string message);
-    }
+		[DllImport("user32")]
+		public static extern int RegisterWindowMessage(string message);
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct MARGINS
+		{
+			public int Left;
+			public int Right;
+			public int Top;
+			public int Bottom;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct DWM_BLURBEHIND
+		{
+			public DWM_BB dwFlags;
+			public bool fEnable;
+			public IntPtr hRgnBlur;
+			public bool fTransitionOnMaximized;
+
+			public DWM_BLURBEHIND(bool enabled)
+			{
+				fEnable = enabled;
+				hRgnBlur = IntPtr.Zero;
+				fTransitionOnMaximized = false;
+				dwFlags = DWM_BB.Enable;
+			}
+
+			public System.Drawing.Region Region
+			{
+				get { return System.Drawing.Region.FromHrgn(hRgnBlur); }
+			}
+
+			public bool TransitionOnMaximized
+			{
+				get => fTransitionOnMaximized;
+				set
+				{
+					fTransitionOnMaximized = value;
+					dwFlags |= DWM_BB.TransitionMaximized;
+				}
+			}
+
+			public void SetRegion(System.Drawing.Graphics graphics, System.Drawing.Region region)
+			{
+				hRgnBlur = region.GetHrgn(graphics);
+				dwFlags |= DWM_BB.BlurRegion;
+			}
+		}
+
+		[Flags]
+		public enum DWM_BB
+		{
+			Enable = 1,
+			BlurRegion = 2,
+			TransitionMaximized = 4
+		}
+
+		[DllImport("dwmapi.dll", EntryPoint = "#131", PreserveSig = false)]
+		public static extern void DwmSetColorizationParameters(ref DWM_COLORIZATION_PARAMS parameters, bool unknown);
+
+		[DllImport("dwmapi.dll", EntryPoint = "#127", PreserveSig = false)]
+		public static extern void DwmGetColorizationParameters(out DWM_COLORIZATION_PARAMS parameters);
+
+		public struct DWM_COLORIZATION_PARAMS
+		{
+			public uint clrColor;
+			public uint clrAfterGlow;
+			public uint nIntensity;
+			public uint clrAfterGlowBalance;
+			public uint clrBlurBalance;
+			public uint clrGlassReflectionIntensity;
+			public bool fOpaque;
+		}
+
+		[DllImport("dwmapi.dll", PreserveSig = false)]
+		public static extern void DwmExtendFrameIntoClientArea(IntPtr hwnd, ref MARGINS margins);
+
+		[DllImport("dwmapi.dll", PreserveSig = false)]
+		public static extern bool DwmIsCompositionEnabled();
+
+		[DllImport("dwmapi.dll")]
+		public static extern void DwmEnableBlurBehindWindow(IntPtr hwnd, ref DWM_BLURBEHIND blurBehind);
+	}
 }
