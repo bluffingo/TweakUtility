2fbacd1b31b12fdc55817b86ad40fc36116c7600
Craftplacer <craftplacer@outlook.de>
2019-09-14 16:13:18 +0200

    Reformat ExtensionsForm.cs

--- TweakUtility/Forms/ExtensionsForm.cs
+++ TweakUtility/Forms/ExtensionsForm.cs
@@ -4,27 +4,29 @@ using System.IO;
 using System.Windows.Forms;
 
 using TweakUtility.Extensions;
+using TweakUtility.Theming;
 
 namespace TweakUtility.Forms
 {
-    internal partial class ExtensionsForm : Form
-    {
-        internal ExtensionsForm() => this.InitializeComponent();
+	internal partial class ExtensionsForm : Form
+	{
+		internal ExtensionsForm() => this.InitializeComponent();
 
-        private void ExtensionsForm_Load(object sender, EventArgs e)
-        {
-            foreach (Extension extension in Program.Loader.Extensions)
-            {
-                var item = new ListViewItem(extension.Name);
+		private void ExtensionsForm_Load(object sender, EventArgs e)
+		{
+			foreach (Extension extension in Program.Loader.Extensions)
+			{
+				var item = new ListViewItem(extension.Name);
 
-                item.SubItems.AddRange(new[] { extension.Description, extension.Author });
+				item.SubItems.AddRange(new[] { extension.Description, extension.Author });
 
-                listView.Items.Add(item);
-            }
-        }
+				listView.Items.Add(item);
+			}
 
-        private void OkButton_Click(object sender, EventArgs e) => this.Close();
+		}
 
-        private void OpenFolderButton_Click(object sender, EventArgs e) => Process.Start(new ProcessStartInfo("explorer.exe", Path.GetFullPath("extensions")) { UseShellExecute = true });
-    }
+		private void OkButton_Click(object sender, EventArgs e) => this.Close();
+
+		private void OpenFolderButton_Click(object sender, EventArgs e) => Process.Start(new ProcessStartInfo("explorer.exe", Path.GetFullPath("extensions")) { UseShellExecute = true });
+	}
 }
