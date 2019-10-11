fb169a66673a6d8083a0bad4d21801a85d3014b5
Craftplacer <craftplacer@outlook.de>
2019-10-12 00:50:59 +0200

    Create EnvironmentVariablesPage.cs

---
+++ TweakUtility/Tweaks/Pages/EnvironmentVariablesPage.cs
@@ -0,0 +1,9 @@
+﻿namespace TweakUtility.Tweaks.Pages
+{
+    internal class EnvironmentVariablesPage : TweakPage
+    {
+        public EnvironmentVariablesPage() : base("Environment Variables")
+        {
+        }
+    }
+}
