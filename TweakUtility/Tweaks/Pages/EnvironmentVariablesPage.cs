e792ab259606e9acdac4af9836be1ad7b25b1c7c
Craftplacer <craftplacer@outlook.de>
2019-10-12 00:51:55 +0200

    Add page view

--- TweakUtility/Tweaks/Pages/EnvironmentVariablesPage.cs
+++ TweakUtility/Tweaks/Pages/EnvironmentVariablesPage.cs
@@ -1,9 +1,9 @@
-﻿namespace TweakUtility.Tweaks.Pages
+﻿using TweakUtility.Tweaks.Views;
+
+namespace TweakUtility.Tweaks.Pages
 {
     internal class EnvironmentVariablesPage : TweakPage
     {
-        public EnvironmentVariablesPage() : base("Environment Variables")
-        {
-        }
+        public EnvironmentVariablesPage() : base("Environment Variables") => this.CustomView = new EnvironmentVariablesPageView();
     }
 }
---
+++ TweakUtility/Tweaks/Views/EnvironmentVariablesPageView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TweakUtility.Tweaks.Views
+{
+    public partial class EnvironmentVariablesPageView : UserControl
+    {
+        public EnvironmentVariablesPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
